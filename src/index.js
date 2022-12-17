c39a6350c5fcbdff610045d1b77d776e931beb62
Naoki <83271349+shimizu-n7@users.noreply.github.com>
2022-12-17 10:03:55 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const onClickAdd = () => {
+  //テキストの値を取得、その後すぐ初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //divの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //listの生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  //未完了のリストにdiv追加
+  document.getElementById("imcomplete-list").appendChild(div);
+};
+
+const e = document.getElementById("add-button");
+e.addEventListener("click", () => onClickAdd(), false);
